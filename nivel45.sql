0b9c0d51a6aebf3770aa94ccb6c02a429ecf2bd7
ELKEVINQ <71275632+ELKEVINQ@users.noreply.github.com>
2024-01-08 08:38:13 -0500

    base?

--- nivel45.sql
+++ nivel45.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-01-2024 a las 14:37:08
+-- Tiempo de generación: 08-01-2024 a las 14:37:52
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
